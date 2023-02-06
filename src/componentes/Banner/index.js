0a25d1137e949a5831222ee43f3a644d15cf5c2a
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-02-06 13:40:20 -0300

    Banner

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,4 +1,6 @@
 import styles from "./Banner.module.css";
+import circuloColorido from  'assets/circulo_colorido.png';
+import minhaFoto from 'assets/minha_foto.png'
 
 export default function Banner() {
    return (
@@ -9,9 +11,22 @@ export default function Banner() {
             </h1>
 
             <p  className={styles.paragrafo}>
-
+                Boas Vindas ao meu blog, Eu sou Leonardo Petruncko tenho 20 anos...
             </p>
+        </div>
 
+        <div className={styles.imagens}>
+            <img
+              className={styles.circuloColorido}
+              src={circuloColorido}
+              aria-hidden={true}
+            />
+
+            <img 
+                className={styles.minhaFoto}
+                src={minhaFoto}
+                alt="Foto do evaldo"
+            />
         </div>
       </div>
    )
