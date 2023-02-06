d9087b4d1b034087138582e473f4af0d83ddbfcd
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-02-06 19:46:33 -0300

    Adicionando posts

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,33 +1,36 @@
+/* eslint-disable jsx-a11y/alt-text */
 import styles from "./Banner.module.css";
-import circuloColorido from  'assets/circulo_colorido.png';
+import circuloColorido from 'assets/circulo_colorido.png';
 import minhaFoto from 'assets/minha_foto.png'
 
+
 export default function Banner() {
-   return (
-      <div className={styles.Banner}>
-        <div className={styles.apresentacao}>
-            <h1 className={styles.titulo}>
-                Olá, Mundo! dale
-            </h1>
+  return (
+    <div className={styles.banner}>
+      <div className={styles.apresentacao}>
+        <h1 className={styles.titulo}>
+          Olá, Mundo!
+        </h1>
 
-            <p  className={styles.paragrafo}>
-                Boas Vindas ao meu blog, Eu sou Leonardo Petruncko tenho 20 anos...
-            </p>
-        </div>
-
-        <div className={styles.imagens}>
-            <img
-              className={styles.circuloColorido}
-              src={circuloColorido}
-              aria-hidden={true}
-            />
-
-            <img 
-                className={styles.minhaFoto}
-                src={minhaFoto}
-                alt="Foto do evaldo"
-            />
-        </div>
+        <p className={styles.paragrafo}>
+          Boas vindas ao meu espaço pessoal! Eu sou Antônio Evaldo, instrutor de Front-end da Alura. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)
+        </p>
       </div>
-   )
+
+      <div className={styles.imagens}>
+       
+        <img
+          className={styles.circuloColorido}
+          src={circuloColorido}
+          aria-hidden={true}
+        />
+
+        <img
+          className={styles.minhaFoto}
+          src={minhaFoto}
+          alt="Foto do Antônio Evaldo sorrindo"
+        />
+      </div>
+    </div>
+  )
 }
