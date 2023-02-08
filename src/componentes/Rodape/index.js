ee357d0b766de212bda6f57478334dc2ad4a2951
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-02-08 15:05:40 -0300

    Rodape

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css'
+import { ReactComponent as MarcaRegistrada } from 'assets/marca_registrada.svg';
+
+export default function Rodape() {
+    return (
+        <footer className={styles.rodape}>
+            <MarcaRegistrada />
+            <p> Desenvolvido por <a href='https://www.instagram.com/idevsolution/'>Leonardo Petruncko</a> </p>
+        </footer>
+    )
+}
