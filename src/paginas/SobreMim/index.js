efeb05b4f54c656f592a45bcdf9c4dd916bcfa24
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-02-09 16:00:45 -0300

    Sobre mimi

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,44 @@
+import styles from './SobreMim.module.css'
+
+import PostModelo from "componentes/PostModelo";
+import fotoCapa from "assets/sobre_mim_capa.png"
+import fotoSobreMim from "assets/sobre_mim_foto.png"
+
 export default function SobreMim() {
   return (
-    <h1>Sobre mim</h1>
+    <PostModelo
+      fotoCapa={fotoCapa}
+      titulo="Sobre mim"
+    >
+      <h3 className={styles.subtitulo}>
+        olá, eu sou o Antônio!
+      </h3>
+
+      <img
+        src={fotoSobreMim}
+        className={styles.fotoSobreMim}
+      />
+
+      <p className={styles.paragrafo}>
+        Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento daleOba casamento daleOba casamento daleOba casamento daleOba casamento dale v v v Oba casamento dale vOba casamento daleOba casamento daleOba casamento daleOba casamento dalev v Oba casamento daleOba casamento dale
+      </p>
+      <p className={styles.paragrafo}>
+        Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento daleOba casamento daleOba casamento daleOba casamento daleOba casamento dale v v v Oba casamento dale vOba casamento daleOba casamento daleOba casamento daleOba casamento dalev v Oba casamento daleOba casamento dale
+      </p>
+      <p className={styles.paragrafo}>
+        Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento daleOba casamento daleOba casamento daleOba casamento daleOba casamento dale v v v Oba casamento dale vOba casamento daleOba casamento daleOba casamento daleOba casamento dalev v Oba casamento daleOba casamento dale
+      </p>
+      <p className={styles.paragrafo}>
+        Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento daleOba casamento daleOba casamento daleOba casamento daleOba casamento dale v v v Oba casamento dale vOba casamento daleOba casamento daleOba casamento daleOba casamento dalev v Oba casamento daleOba casamento dale
+      </p>
+      <p className={styles.paragrafo}>
+        Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento daleOba casamento daleOba casamento daleOba casamento daleOba casamento dale v v v Oba casamento dale vOba casamento daleOba casamento daleOba casamento daleOba casamento dalev v Oba casamento daleOba casamento dale
+      </p>
+      <p className={styles.paragrafo}>
+        Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento dale Oba casamento daleOba casamento daleOba casamento daleOba casamento daleOba casamento dale v v v Oba casamento dale vOba casamento daleOba casamento daleOba casamento daleOba casamento dalev v Oba casamento daleOba casamento dale
+      </p>
+
+    </PostModelo>
+
   )
 }
