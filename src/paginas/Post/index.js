540a94c361cc3b045feb9e89587fae689542f96c
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-03-07 00:24:34 -0300

    CSS & Markdown

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom"
 import posts from "json/posts.json"
 import PostModelo from "componentes/PostModelo"
+import { ReactMarkdown } from "react-markdown/lib/react-markdown"
+import './Post.css';
 
 export default function Post() {
 
@@ -14,10 +16,16 @@ export default function Post() {
 
     return (
         <PostModelo
-        fotoCapa={`/assets/posts/${post.id}/capa.png`}
-        titulo={post.titulo}
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
         >
-            Texto...
+            <div className="post-markdown-container">
+                <ReactMarkdown>
+                    {post.texto}
+                </ReactMarkdown>
+            </div>
+
+
         </PostModelo>
     )
 }
